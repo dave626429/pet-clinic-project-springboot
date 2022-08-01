ce74bd60084b4a1e782288b9c038eb23be8ac0c7
Davinder Singh <dave626429@gmail.com>
2022-08-01 19:21:33 +0530

    adding controllers for views

---
+++ java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.testapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+	
+	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
+	public String getOwners() {
+		return "owners/index";
+	}
+
+}
---
+++ java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/controllers/PetController.java
@@ -0,0 +1,14 @@
+package com.testapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PetController {
+	
+	@RequestMapping({"/pets","/pets/index","/pets/index.html"})
+	public String getPets() {
+		return "pets/index";
+	}
+
+}
