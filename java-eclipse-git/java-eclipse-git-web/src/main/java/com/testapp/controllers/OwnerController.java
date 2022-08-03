1d90c83426ee512b8d10300e39db3f20c91209f7
Davinder Singh <dave626429@gmail.com>
2022-08-03 20:40:48 +0530

    owners view from h2db completed

--- java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/controllers/OwnerController.java
+++ java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package com.testapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.testapp.services.OwnerServices;
 
 @Controller
 public class OwnerController {
 	
+	private final OwnerServices ownerservice;
+	
+	@Autowired
+	public OwnerController(OwnerServices ownerservice) {
+		super();
+		this.ownerservice = ownerservice;
+	}
+
 	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
-	public String getOwners() {
+	public String getOwners(Model model) {
+		model.addAttribute("owners",ownerservice.findAll());
 		return "owners/index";
 	}
 
