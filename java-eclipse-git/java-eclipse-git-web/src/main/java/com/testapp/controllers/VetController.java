28eadc0cf5512f5b528df3362dea1bc3af9fbeba
Davinder Singh <dave626429@gmail.com>
2022-08-03 20:45:52 +0530

    vets view from h2db

--- java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/controllers/VetController.java
+++ java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/controllers/VetController.java
@@ -1,13 +1,26 @@
 package com.testapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.testapp.services.VetServices;
 
 @Controller
 public class VetController {
 	
+	private final VetServices vetServices;
+	
+	@Autowired
+	public VetController(VetServices vetServices) {
+		super();
+		this.vetServices = vetServices;
+	}
+	
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String getVets() {
+	public String getVets(Model model) {
+		model.addAttribute("vets", vetServices.findAll());
 		return "vets/index";
 	}
 
