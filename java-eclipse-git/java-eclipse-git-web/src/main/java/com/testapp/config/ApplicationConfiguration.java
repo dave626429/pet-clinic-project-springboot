cf0c65fd3125901abae504ba76c0cd8d510f8fd4
Davinder Singh <dave626429@gmail.com>
2022-08-03 23:05:24 +0530

    minor changes

--- java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/config/ApplicationConfiguration.java
+++ java-eclipse-git/java-eclipse-git-web/src/main/java/com/testapp/config/ApplicationConfiguration.java
@@ -3,7 +3,6 @@ package com.testapp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import com.testapp.datasource.FakeDataSource;
 
@@ -18,3 +17,4 @@ public class ApplicationConfiguration {
 	}
 
 }
+	
